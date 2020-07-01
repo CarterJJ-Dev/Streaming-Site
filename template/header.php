28023f5ceab30e31006f165cb548b8adbd33669f
CarterJJ-Dev <67302213+CarterJJ-Dev@users.noreply.github.com>
2020-07-01 03:51:28 -0500

    Create header.php

---
+++ template/header.php
@@ -0,0 +1,37 @@
+<?php
+require('include/db.php');
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans:400,700|Rozha+One|Great+Vibes" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="template/css/style.css" />
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+<a class="navbar-brand" href="index.php">Streaming-Site</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item active">
+      <a class="nav-link" href="browse.php">Browse</a>
+    </li>
+  </ul>
+</div>
+</nav>
