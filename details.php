f0c07f49f7cde962140930ac5a5e58d1c35be3f4
CarterJJ-Dev <67302213+CarterJJ-Dev@users.noreply.github.com>
2020-07-01 03:50:38 -0500

    Create details.php

---
+++ details.php
@@ -0,0 +1,41 @@
+<?php
+include('template/header.php');
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $result = $mysqli->query("SELECT * FROM movies WHERE id=" . $id) or die($mysqli->error);
+}
+?>
+
+<div class="container">
+  <div class="row">
+    <?php
+      while($row = $result->fetch_assoc()) {
+        $name = $row["name"]; $plot = $row["plot"]; $cast = $row["cast"]; $year = $row["year"]; $runtime = $row["runtime"]; $rated = $row["rated"]; $rating = $row["rating"];
+        $imdb = $row["imdb"]; $img = $row["img"]; $trailer = $row["trailer"]; $file_name = $row["file_name"];
+    ?>
+        <div class="col-xl-4 col-md-12">
+          <img class="card-img-top mx-auto d-block" src="<?php echo $img; ?>" style="width: 16rem">
+          <div class=" mx-auto d-block text-center" style="width: 16rem;">
+            <a href="play.php" class="btn btn-primary mt-2">Play</a>
+            <a href="https://www.redirect.am/?https://www.youtube.com/watch?v=<?php echo $trailer; ?>" class="btn btn-primary mt-2">Trailer</a>
+            <a href="https://www.redirect.am/?https://www.imdb.com/title/<?php echo $imdb; ?>" class="btn btn-primary mt-2">IMDb</a>
+          </div>
+        </div>
+
+        <div class="col-xl-8 col-md-12">
+          <?php
+          echo "<h2 class=\"text-center\">" . $name . " (" . $year . ")</h2><br>";
+          echo "<p> <b>Plot</b> <br>" . $plot . "</p>";
+          echo "<p> <b>Cast</b> <br>" . $cast . "</p>";
+          echo "<b>Runtime  </b>" . $runtime . "<b>  Rated  </b>" . $rated . "<b>  Rating  </b>" . $rating;
+          ?>
+        </div>
+
+<?php } ?>
+  </div>
+</div>
+
+<?php
+include('template/footer.php');
+?>
