1ed3383aff61c093433eac8b999b3ddad78d5921
CarterJJ-Dev <67302213+CarterJJ-Dev@users.noreply.github.com>
2020-07-13 06:13:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <?php
+
+// Display header
 include('template/header.php');
-$result = $mysqli->query("SELECT id, name, img FROM movies") or die($mysqli->error);
-?>
 
+// Pull movie results from database and limit to 20
+$result = $mysqli->query("SELECT id, name, img, file_name FROM movies LIMIT 20") or die($mysqli->error);
 
-<div class="container">
-  <div class="row">
-    <?php
-      while($row = $result->fetch_assoc()) {
-        $id = $row["id"]; $name = $row["name"]; $img = $row["img"];
-    ?>
-    <div class="card" style="width: 12rem;">
-      <img class="card-img-top" src="<?php echo $img; ?>" alt="Card image cap">
-      <div class="card-body text-center">
-        <h5 class="card-title"><?php echo $name; ?></h5>
-        <a href="play.php" class="btn btn-primary">Watch</a>
-        <a href="details.php?id=<?php echo $id; ?>" class="btn btn-primary">Info</a>
-      </div>
-  </div>
-<?php } ?>
-  </div>
-</div>
+// Display movie results
+include('template/list.template.php');
 
-
-
-<?php
+// Display footer
 include('template/footer.php');
 ?>
